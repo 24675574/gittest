bda09a7fb55cdbea87ce1c936f4db6ac2a4ab4e7
聂钢 <gangnie@iflytek.com>
2021-08-22 10:18:33 +0800

    dev first commit

--- src/main/java/com/gangnie/git/App.java
+++ src/main/java/com/gangnie/git/App.java
@@ -1,9 +1,17 @@
 package com.gangnie.git;
 
-public class App {
 
+/**
+ * 这是在master添加的注释
+ */
+public class App {
+    /**
+     * 这是在dev分支中添加的注释
+     */
     public static void main(String[] args) {
         System.out.println("hello idea!");
         System.out.println("hello git!");
+
+        System.out.println("dev branch add");
     }
 }
