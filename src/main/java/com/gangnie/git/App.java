885ea949f3ead7ebaaae45c9bb59fbf410eb6f13
聂钢 <gangnie@iflytek.com>
2021-08-22 10:01:51 +0800

    注释提交

--- src/main/java/com/gangnie/git/App.java
+++ src/main/java/com/gangnie/git/App.java
@@ -1,7 +1,13 @@
 package com.gangnie.git;
 
-public class App {
 
+/**
+ * 这是在master添加的注释
+ */
+public class App {
+    /**
+     * 这是在dev分支中添加的注释
+     */
     public static void main(String[] args) {
         System.out.println("hello idea!");
         System.out.println("hello git!");
