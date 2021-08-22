3d4bf1c325d3884f580c27afa1e07e5843dea44a
聂钢 <gangnie@iflytek.com>
2021-08-22 09:53:28 +0800

    第二次提交

--- src/main/java/com/gangnie/git/App.java
+++ src/main/java/com/gangnie/git/App.java
@@ -1,4 +1,9 @@
 package com.gangnie.git;
 
 public class App {
+
+    public static void main(String[] args) {
+        System.out.println("hello idea!");
+        System.out.println("hello git!");
+    }
 }
